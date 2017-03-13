0ab00bd49915c69e94cccc368ab89c834d392884
Tomas Olvecky <tolvecky@frinx.io>
2017-03-13 15:20:28 +0100

    Add jobdsl_hello_world_build.groovy
    Change-Id: I242c3b51f26091c32ef9ecd1676c6eaf8af7f7f2

---
+++ jobs/jobdsl_hello_world_build.groovy
@@ -0,0 +1,39 @@
+def gitUrl = 'ssh://admin@gerrit:29418/hello-world-samples'
+def restrictedByLabel = 'docker-agent'
+def inputCredentialsId = 'root'
+
+
+Map branchesToAltRepos = [
+  'beryllium/release':'"release::default::http://nexus:8081/repository/maven-releases"',
+  'beryllium/development':'"snapshot::default::http://nexus:8081/repository/maven-snapshots"',
+]
+
+branchesToAltRepos.each {branchName,altRepo->
+  job('dsl-hello-world-' + branchName.replaceAll('/','_')) {
+    label(restrictedByLabel)
+    scm {
+      git {
+        remote {
+          name('origin')
+          url(gitUrl)
+          credentials(inputCredentialsId)        
+        }
+        branch(branchName)
+      }
+    }
+    triggers {
+      scm('* * * * *')
+    }
+    steps {
+      maven{
+        goals('''
+  -B 
+  -Ddocker_registry_address_with_slash=nexus.localhost:8082/
+  help:effective-pom deploy 
+  -P docker 
+  -DaltDeploymentRepository=''' + altRepo)
+        providedGlobalSettings('MyGlobalSettings')
+      }
+    }
+  }
+}
